65cd61d696f28291d993915bad7e76b6869bc2a7
MohAlshahrani <90883554+MohAlshahrani@users.noreply.github.com>
2023-02-22 14:03:23 -0500

    parse objects changed

--- data_curation/parse_objects.m
+++ data_curation/parse_objects.m
@@ -11,18 +11,25 @@ function parse_objects(root, folder_index)
     i = folder_index + 3;
     
 
-    root_imgs = [root '/dataset'];
+    root_imgs = [root '/data'];
     dir_root_imgs = dir(root_imgs);
     dir_root_imgs = {dir_root_imgs.name};
+%     disp(dir_root_imgs);
     % we always start from 3 because at 1 we have '.' and at 2 '..'
     level1 = [root_imgs slash dir_root_imgs{i}];
+%     level1 = root_imgs;
+%     slash dir_root_imgs{i}];
+    
     dir_level1 = dir(level1);
+    
     dir_level1 = {dir_level1.name};
+%     disp(dir_level1);
     dir_root_imgs_i = dir_root_imgs{i};
     % j iterates across videos
-    for j=3:numel(dir_level1)
+    for j=4:numel(dir_level1)
         level2 = [root_imgs slash dir_root_imgs_i slash dir_level1{j}];
         dir_level2 = dir(level2);
+%         disp(dir_level2);
         dir_level2 = {dir_level2.name};
         if isfolder(level2)
             new_file_video = [level2 '.txt'];
@@ -30,21 +37,22 @@ function parse_objects(root, folder_index)
             % create new file per video which lists all the objects
                 fv = fopen(new_file_video, 'w');
                 % k iterates across frames
-                for k=3:numel(dir_level2)
+                for k=4:numel(dir_level2)
                     fprintf('Processing i: %d j: %d k: %d\n', i-2, j-2, k-2);
                     path = [root_imgs slash dir_root_imgs_i slash dir_level1{j} slash dir_level2{k}];
+%                     disp(path);
                     if ismember('.txt', path)
                         fin = fopen(path,'r');
                         % reading i, j, k, o, trackid, o_class, frame_sz(2), frame_sz(1), o_xmin, o_ymin, o_sz(2), o_sz(1), im_path
                         C = textscan(fin, '%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%s');
                         fclose(fin);
-
-                        if ~isempty(C{8})
+%                            disp(C);
+%                         if ~isempty(C{8})
                             n_lines = numel(C{1});
                             trackids = C{5};
                             classes = C{6};
-                            frame_w = C{7}(1);
-                            frame_h = C{8}(1);
+                            frame_w = C{7};%(1);
+                            frame_h = C{8};%(1);
                             o_xmins = C{9};
                             o_ymins = C{10};
                             o_ws = C{11};
@@ -52,11 +60,13 @@ function parse_objects(root, folder_index)
                             frame_paths = C{13};
 
                             for o=1:n_lines
-                                if ~isempty(C{4}(o))
+                                disp(C{4});
+%                                 if ~isempty(C{4}(o))
+%                                     disp('gt');
                                     fprintf(fv, '%d,%d,%d,%d,%d,%d,%d,%d,%s\n', trackids(o), classes(o), frame_w, frame_h, o_xmins(o), o_ymins(o), o_ws(o), o_hs(o), frame_paths{o});
-                                end
+%                                 end
                             end
-                        end
+%                         end
                     end
                 end
                 fclose(fv);
